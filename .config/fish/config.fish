29bb51a00e4a756d2de20c903072738b9714846b
Chris Brown <christopher.brown@genomicsengland.co.uk>
2021-09-24 15:48:44 +0100

    Initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+starship init fish | source
+
+status is-login; and pyenv init --path | source
+status is-interactive; and pyenv init - | source
+status is-interactive; and pyenv virtualenv-init - | source
+
