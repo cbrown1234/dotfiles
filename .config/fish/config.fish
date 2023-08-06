b44afba19c8e9eab826153a784c483a1369a8e50
Chris Brown <cbrown1234@hotmail.co.uk>
2023-08-06 15:37:37 +0100

    Add pipx bin location

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,5 @@ status is-login; and pyenv init --path | source
 status is-interactive; and pyenv init - | source
 status is-interactive; and pyenv virtualenv-init - | source
 
+# pipx
+fish_add_path /home/chris/.local/bin
