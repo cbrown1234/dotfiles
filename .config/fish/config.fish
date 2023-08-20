9972f5daa5cee1f90f90ebfc37dabe5658253b10
Chris Brown <cbrown1234@hotmail.co.uk>
2023-08-20 19:30:25 +0100

    Add pyenv path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,8 @@
 starship init fish | source
 
+# pyenv
+fish_add_path /home/chris/.pyenv/bin
+
 status is-login; and pyenv init --path | source
 status is-interactive; and pyenv init - | source
 status is-interactive; and pyenv virtualenv-init - | source
