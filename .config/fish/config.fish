46fbaeba36d1e512191460e23906377b6d1343ee
Chris Brown <cbrown1234@hotmail.co.uk>
2023-08-21 21:46:58 +0100

    Add default tmux session

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,12 @@
+if status is-interactive
+and not set -q TMUX
+    if tmux has-session -t home
+        exec tmux attach-session -t home
+    else
+        tmux new-session -s home
+    end
+end
+
 starship init fish | source
 
 # pyenv
