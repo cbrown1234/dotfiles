21a32e8f7d7bf3d09ce41bd5e367c1a3c010c417
Chris Brown <christopher.brown@genomicsengland.co.uk>
2024-12-18 09:05:27 +0000

    Add config from work

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -37,10 +37,17 @@ end
 # brew
 if test -f /home/linuxbrew/.linuxbrew/bin/brew
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+    if test -d (brew --prefix)"/share/fish/completions"
+        set -p fish_complete_path (brew --prefix)/share/fish/completions
+    end
+
+    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+        set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+    end
 end
 
 # direnv
 if command -q direnv
     direnv hook fish | source
 end
-
